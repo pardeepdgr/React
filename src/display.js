17d8848bf962be8dbf386f86b63c2c77ba80cd0c
Pardeep Dogra <pardeepdgr@gmail.com>
2018-04-15 22:18:47 +0530

    create click counter app

---
+++ src/display.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Display = (props) => {
+    return(
+        <div>{props.count}</div>
+    );
+}
+
+export default Display;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Display from './display';
+import Button from './button';	
 
 class App extends React.Component {
-	
+	state = { counter: 0 };
+
+	inc = () => {
+    	this.setState((previousState) => {
+        	return {counter: previousState.counter + 1}
+    	});
+	};
+
 	render() {
   	return(
     	<div>
-    		React Seed
+			<Display count = {this.state.counter} />
+    		<Button incCount = {this.inc} />
       </div>
     );
   }
